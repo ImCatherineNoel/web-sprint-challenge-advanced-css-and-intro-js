ca1418cebe8021885615114a77841317697b1a7d
Catherine Tucker <ImCatherineNoel@gmail.com>
2020-05-29 20:15:04 -0700

    Completed : 2b: Tasks 1- 3, answered ?s in readme

--- index.js
+++ index.js
@@ -207,12 +207,15 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+// console.log(artists[0].name);
+// console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+// console.log(artists[8].name);
+// artists[8].name = "Vincent van Gough"
+// console.log(artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,20 +225,34 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
-  }
+// function getArtistByIndex(id, name) {
+//   const array= name;
+//   const aname= array[id].name;
+//   return(`The artist at index ${id} is ${aname}`);
+// }
+// console.log(getArtistByIndex(0, artists));
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1900-2000) */
 
-function get20s(/* Code here */){
+function get20s(botYear, topYear){
+  const bYear = botYear;  //earlier year cap
+  const tYear = topYear;  //later year cap
 
-  /* Code here */
-
+let index = 0;
+for (index in artists){
+  if (artists[index].years > bYear && artists[index].years < tYear){
+      console.log(artists[index].name);
+      index++;
+}    
+  else  {}
+    index++;
 }
+
+
+
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
